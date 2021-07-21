90c46e8089a8ff0f81090536ad21ae0b98296a6c
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-07-21 11:48:45 +0200

    Add optional updateTheme options parameter

--- src/theme/updateTheme.ts
+++ src/theme/updateTheme.ts
@@ -5,10 +5,29 @@ import { updateEditor } from "../editor";
 import { exportConfig } from "../export";
 import { merge, RecursivePartial } from "../util";
 
-export function updateTheme(theme: RecursivePartial<Theme>) {
+interface UpdateThemeOptions {
+    noExport: boolean;
+}
+
+const DEFAULT_UPDATE_THEME_OPTIONS: UpdateThemeOptions = {
+    noExport: false,
+};
+
+export function updateTheme(
+    theme: RecursivePartial<Theme>,
+    opts?: Partial<UpdateThemeOptions>,
+) {
+    const { noExport } = {
+        ...DEFAULT_UPDATE_THEME_OPTIONS,
+        ...opts,
+    };
+
     const context = getContext();
     context.theme = merge(context.theme, theme);
     applyTheme(theme);
     updateEditor(context.theme);
-    exportConfig(context);
+
+    if (!noExport) {
+        exportConfig(context);
+    }
 }
